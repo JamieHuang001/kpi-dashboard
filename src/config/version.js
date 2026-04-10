acf9a78a35e85e80373a64aa24c3c2b80a58d24a
jamiehuang001 <hwaije09@gmail.com>
2026-04-10 14:47:43 +0800

    更新版號

--- src/config/version.js
+++ src/config/version.js
@@ -6,6 +6,6 @@
 // ★ 發版時只需修改此檔案
 // =====================================================
 
-export const APP_VERSION = '6.0.1';
+export const APP_VERSION = '6.0.2';
 export const APP_VERSION_DISPLAY = `V${APP_VERSION}`;
 export const APP_TITLE = 'BI Dashboard';
